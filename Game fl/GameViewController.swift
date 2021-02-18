262507a8513d5a93eb865851f7ee7f97e8bbcfec
VorchunV <varenkoluchka@gmail.com>
2021-02-18 18:20:58 +0300

    Lesson 1

---
+++ Game fl/GameViewController.swift
@@ -0,0 +1,145 @@
+//
+//  GameViewController.swift
+//  Game fl
+//
+//  Created by Варвара Фомина on 18.02.2021.
+//
+
+//import UIKit
+//import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+    
+    //Add ship to the scene
+    func addShip() {
+        //Get a scene with the ship
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // Find ship in the scene
+        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        //Set ship coordinates
+        ship.position.z = -105
+        // Make the ship fly from far to the origin
+        ship.runAction(SCNAction.move(to: SCNVector3(), duration: 5)) {
+            print(#line, #function, "GAME OVER")
+            ship.removeFromParentNode()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            self.addShip()
+            }
+        }
+        // Get the scene
+        let scnView = self.view as! SCNView
+        //Add ship to the scene
+        scnView.scene?.rootNode.addChildNode(ship)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+//        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the ship node
+        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        ship.removeFromParentNode()
+    
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+        
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        scnView.addGestureRecognizer(tapGesture)
+
+        // Add the ship to the scene
+        addShip()
+}
+
+        
+    @objc func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.location(in: scnView)
+        let hitResults = scnView.hitTest(p, options: [:])
+        // check that we clicked on at least one object
+        if hitResults.count > 0 {
+            // retrieved the first clicked object
+            let result = hitResults[0]
+            
+            // get its material
+            let material = result.node.geometry!.firstMaterial!
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.animationDuration = 0.5
+            
+            // on completion - unhighlight
+            SCNTransaction.completionBlock = {
+                SCNTransaction.begin()
+                SCNTransaction.animationDuration = 0.5
+                
+                material.emission.contents = UIColor.black
+                
+                SCNTransaction.commit()
+            }
+            
+            material.emission.contents = UIColor.red
+            
+            SCNTransaction.commit()
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+}
